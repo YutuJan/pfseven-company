6f7203717a316970a09eecf52a74faa04c056f25
Sirinian Aram Emmanouil <ae.sirinian@gmail.com>
2021-12-06 15:40:22 +0200

    Refactored DB Tables

--- pf7-basic-functionality/src/main/java/com/pfseven/eshop/model/DatabaseService.java
+++ pf7-basic-functionality/src/main/java/com/pfseven/eshop/model/DatabaseService.java
@@ -17,8 +17,6 @@ public class DatabaseService {
     private Statement statement;
 
     public void launchDB() throws SQLException {
-        int result;
-
         server = Server.createTcpServer("-tcpAllowOthers", "-tcpDaemon");
         server.start();
         logger.info("Server started... " + server.getStatus());
@@ -29,21 +27,45 @@ public class DatabaseService {
         connection = DriverManager.getConnection(DB_CONNECTION_URL_MEMORY_MODE, DB_USERNAME, DB_PASSWORD);
         statement = connection.createStatement();
 
+        createCustomersTable();
+        createProductsTable();
+        createOrdersTable();
+    }
 
-        result = statement.executeUpdate("create table orders (" +
-                " customer_id varchar not null, " +
-                " customer_name varchar(50) not null, " +
-                " customer_category varchar not null, " +
-                " product_id varchar not null primary key, " +
-                " product_name varchar(50) not null, " +
-                " product_cost varchar not null, " +
-                " product_cost_with_discount varchar not null)");
+    private void createCustomersTable() throws SQLException {
+        int result;
+
+        result = statement.executeUpdate("create table customers(" +
+                "customer_id varchar not null," +
+                "customer_name varchar(50) not null," +
+                "customer_category varchar not null" +
+                ");");
         logger.info("Created table command was successful with result {}.", result);
+    }
 
-        result = statement.executeUpdate("create table products (" +
-                " product_id varchar not null , " +
-                " product_name varchar(50) not null , " +
-                " product_cost varchar not null)");
+    private void createProductsTable() throws SQLException {
+        int result;
+
+        result = statement.executeUpdate("create table products(" +
+                "product_id varchar not null," +
+                "product_name varchar(50) not null," +
+                "product_price double not null" +
+                ");");
+        logger.info("Created table command was successful with result {}.", result);
+    }
+
+    private void createOrdersTable() throws SQLException {
+        int result;
+
+        result = statement.executeUpdate("create table orders(" +
+                "order_id varchar not null," +
+                "customer_id varchar not null," +
+                "product_id varchar not null," +
+                "payment_method varchar not null," +
+                "cost_with_discount double not null," +
+                "foreign key (customer_id) references customers(customer_id)," +
+                "foreign key (product_id) references products(product_id)" +
+                ");");
         logger.info("Created table command was successful with result {}.", result);
     }
 
